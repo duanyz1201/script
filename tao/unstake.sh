f122112ef8aab338b849e62a62a0b27d01695312
duanyz <yingzhong.dyz@gmail.com>
2025-05-08 18:17:37 +0800

    更新撤销质押脚本，增加超时设置，优化密码处理逻辑，并记录操作日志

--- tao/unstake.sh
+++ tao/unstake.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+log() {
+    local level=$1
+    shift
+    local message=$@
+    local timestamp=$(date +"%FT%T.%3N")
+    echo "$timestamp $level - $message" >> /root/duanyz/logs/unstake.log
+}
+
+script_path="/root/duanyz/unstake.exp"
+unstake_output_dir="/root/duanyz/logs/unstake_output"
+
 wallets=(
-    "51 sg-5 5DPB6bPAqBC7JMBMzdwyA4k4WsreoGpHkixZh1QZS7R9pFyr 123456"
+    "51 51-baiz02 5DA1em5gQmCdoMsc6QUMwhqLRE5byFULbePPc7isbhuX7Wc8 123456"
 )
 
 for wallet in "${wallets[@]}"; do
@@ -10,14 +21,25 @@ for wallet in "${wallets[@]}"; do
     hotkey=$(echo "$wallet" | awk '{print $3}')
     password=$(echo "$wallet" | awk '{print $4}')
 
-    export hotkey
+    export netuid
     export wallet_name
+    export hotkey
     export password
-    echo "Hotkey: ${hotkey}, Wallet: ${wallet_name}, Password: ${password}"
+    log INFO "Netuid: ${netuid}, Hotkey: ${hotkey}, Wallet: ${wallet_name}, Password: ${password}"
 
-   # /usr/bin/expect ./unstake.exp > output_${wallet_name}.txt 2>&1
+    /usr/bin/expect ${script_path} > ${unstake_output_dir}/output_${wallet_name}.txt 2>&1
 
-   # Received=$(grep -A 4 "Received (Τ)" output_${wallet_name}.txt | tail -n 1 | awk '{print $18}')
+    Received=$(grep -A 4 "Received (τ)" ${unstake_output_dir}/output_${wallet_name}.txt | tail -n 1 | awk '{print $16}')
+    if [[ -z $Received ]]; then
+    log ERROR "No 'Received' value found for wallet: ${wallet_name}"
+    continue
+    fi
 
-   # echo "Wallet: ${wallet_name}, Hotkey: ${hotkey}, Received: ${Received}"
+    resolt=$(curl --location 'https://api.umpool.io/api/v1/tao/unbond/save' --header 'Content-Type: application/json' --data '{"hotkey": "'$hotkey'","unbond": '${Received}'}')
+    resolt_code=$(echo $resolt | jq -r '.code')
+    if [[ $resolt_code -eq 200 ]]; then
+        log INFO "Unbond successful for wallet: ${wallet_name}, Hotkey: ${hotkey}, Received: ${Received}"
+    else
+        log ERROR "Unbond failed for wallet: ${wallet_name}, Error code: ${resolt_code}"
+    fi
 done
