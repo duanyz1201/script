4ab0b1127ac46a286ceaf730c2dfdaa2d46f762d
duanyz <yingzhong.dyz@gmail.com>
2025-05-12 14:44:10 +0800

    更新撤销质押脚本，优化API请求逻辑，增加错误日志记录，并调整钱包信息

--- tao/unstake.sh
+++ tao/unstake.sh
@@ -13,10 +13,27 @@ log() {
 script_path="/root/duanyz/unstake.exp"
 unstake_output_dir="/root/duanyz/logs/unstake_output"
 
+online_url="https://api.umpool.io/api/v1/tao/unbond/save"
+test_url="http://172.28.56.107:12001/api/v1/tao/unbond/save"
+
+declare -A wallet_api_map=(
+    ["zw-20"]=$online_url
+    ["sg-5"]=$online_url
+    ["51-baiz02"]=$online_url
+    ["chen"]=$online_url
+    ["51-baiz03"]=$online_url
+    ["sc-11-a100"]=$test_url
+    ["sc-7-h100"]=$test_url
+)
+
 wallets=(
     "51 51-baiz02 5DA1em5gQmCdoMsc6QUMwhqLRE5byFULbePPc7isbhuX7Wc8 123456"
-    "64 sg-5 5DPB6bPAqBC7JMBMzdwyA4k4WsreoGpHkixZh1QZS7R9pFyr 123456"
+    "51 51-baiz03 5CAPKyuPdqYZFjd6nVQnpk1JuhAaEm3vaGvprNU6zHBnBf3Z 123456"
     "51 chen 5CzR9HVq2yUCvnrgDxM6zp4MvQRTQu7y2m4mQ4C3E6Aoceh3 A123.com"
+    "64 sg-5 5DPB6bPAqBC7JMBMzdwyA4k4WsreoGpHkixZh1QZS7R9pFyr 123456"
+    "64 zw-20 5Ev5zc4wGtZEheyT3PviYHMLi1yU7sovyrDSxUvdkKe4mHL1 123456"
+    "64 sc-11-a100 5C5rp9HcVTUn963AET4VpsVA4azawYxrQnuF39hkTUEopYZ4 123456"
+    "64 sc-7-h100 5DqTcmSHHBzy59EoJgZVQH5XsoiS4Sz4PVdX17pLWQHxzTyh 123456"
 )
 
 for wallet in "${wallets[@]}"; do
@@ -39,7 +56,19 @@ for wallet in "${wallets[@]}"; do
     continue
     fi
 
-    resolt=$(curl --location 'https://api.umpool.io/api/v1/tao/unbond/save' --header 'Content-Type: application/json' --data '{"hotkey": "'$hotkey'","unbond": '${Received}'}')
+    api_url=${wallet_api_map[$wallet_name]}
+    if [[ -z $api_url ]]; then
+        log ERROR "No API URL found for wallet: ${wallet_name}"
+        continue
+    fi
+    log INFO "Wallet Name : $wallet_name, API URL: ${api_url}"
+
+    resolt=$(curl --location ${api_url} --header 'Content-Type: application/json' --data '{"hotkey": "'$hotkey'","unbond": '${Received}'}')
+    if [[ $? -ne 0 || -z $resolt ]]; then
+        log ERROR "API request failed for wallet: ${wallet_name}, Hotkey: ${hotkey}"
+        continue
+    fi
+        
     resolt_code=$(echo $resolt | jq -r '.code')
     if [[ $resolt_code -eq 200 ]]; then
         log INFO "Unbond successful for wallet: ${wallet_name}, Hotkey: ${hotkey}, Received: ${Received}"
